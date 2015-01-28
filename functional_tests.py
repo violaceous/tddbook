6322228277e6b79c2beae5ad778966c9bd18e4c6
violaceous <bodhisnatcher@gmail.com>
2015-01-28 11:51:59 -0500

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -15,17 +16,32 @@ class NewVisitorTest(unittest.TestCase):
         
         # Rico notes at the header of the page for he is all seeing
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        # Rico is prompted to enter a to-do
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'), 'Enter a to-do item'
+        )
+
+        # Rico enters 'dig a rico hole'
+        inputbox.send_keys('dig a rico hole')
+        
+        # Rico hits enter and the page updates
+        # it now has '1: dig a rico hole'
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: dig a rico hole' for row in rows)
+        )
+
         self.fail('Finish the test!')
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
-
-# Rico is prompted to enter a to-do
-
-# Rico types 'dig a rico hole'
-
-# Rico hits enter and the page updates
-# it now has '1: dig a rico hole'
 
 # Rico enters another item 
 # it is 'swallow the sadness'
