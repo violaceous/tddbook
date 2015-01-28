d58540644ab21b0b70090d18c9c433fe7aa715e0
violaceous <bodhisnatcher@gmail.com>
2015-01-28 11:21:46 -0500

    added unit test and url mapping to homepage

--- lists/tests.py
+++ lists/tests.py
@@ -7,3 +7,10 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>Rico and the unending To-Do list</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
