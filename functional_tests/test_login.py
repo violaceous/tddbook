14fafa28a74943ac75ffa8b5406d8585898cbb3e
violaceous <bodhisnatcher@gmail.com>
2015-02-19 14:14:38 -0500

    add logout functionality

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -17,9 +17,22 @@ class LoginTest(FunctionalTest):
 
     def wait_for_element_with_id(self, element_id):
         WebDriverWait(self.browser, timeout=30).until(
-            lambda b: b.find_element_by_id(element_id)
-        )
+            lambda b: b.find_element_by_id(element_id),
+            'Could not find element with id {}. Page text was {}'.format(
+                element_id, self.browser.find_element_by_tag_name('body').text
+                )
+            )
 
+    def wait_to_be_logged_in(self):
+        self.wait_for_element_with_id('id_logout')
+        navbar = self.browser.find_element_by_css_selector('.navbar')
+        self.assertIn('sexy_rico@mockmyid.com', navbar.text)
+
+    def wait_to_be_logged_out(self):
+        self.wait_for_element_with_id('id_login')
+        navbar = self.browser.find_element_by_css_selector('.navbar')
+        self.assertNotIn('sexy_rico@mockmyid.com', navbar.text)
+        
     def test_login_with_persona(self):
         # Rico goes to the list site
         # and notices he can sign in
@@ -41,6 +54,16 @@ class LoginTest(FunctionalTest):
         self.switch_to_new_window('To-Do')
 
         # Rico can see that he is logged in
-        self.wait_for_element_with_id('id_logout')
-        navbar = self.browser.find_element_by_css_selector('.navbar')
-        self.assertIn('sexy_rico@mockmyid.com', navbar.text)
+        self.wait_to_be_logged_in()
+
+        # Rico refreshes the page and sees he is still logged in
+        self.browser.refresh()
+        self.wait_to_be_logged_in()
+
+        # Rico logs out
+        self.browser.find_element_by_id('id_logout').click()
+        self.wait_to_be_logged_out()
+
+        # Rico refreshes to check he is really logged out
+        self.browser.refresh()
+        self.wait_to_be_logged_out()
